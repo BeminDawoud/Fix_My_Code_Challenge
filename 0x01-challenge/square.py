cf2e8be9cf4bfd9c74490c29b11af3107fc0feb3
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-15 19:19:17 +0300

    Fix Square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,25 +1,57 @@
 #!/usr/bin/python3
 
-class Square:
-    
+
+class square:
+
+    width = 0
+    height = 0
+
     def __init__(self, *args, **kwargs):
-        self.width = kwargs.get('width', 0)
-        self.height = kwargs.get('height', 0)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """Area of the square"""
+        if self.width == 0 and self.height == 0:
+            return 0
 
-    def perimeter_of_my_square(self):
-        """ Perimeter of the square """
-        return self.width * 4
+        if self.width == 0:
+            return self.height * self.height
+
+        if self.height == 0:
+            return self.width * self.width
+
+        return self.width * self.height
+
+    def PermiterOfMySquare(self):
+        """Perimeter of the square"""
+        if self.width == 0 and self.height == 0:
+            return 0
+
+        if self.width == 0:
+            return 4 * self.height
+
+        if self.height == 0:
+            return 4 * self.width
+
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        if self.width == 0 and self.height == 0:
+            return "0/0"
+
+        if self.width == 0:
+            return "0/{}".format(self.height)
+
+        if self.height == 0:
+            return "{}/0".format(self.width)
+
+        return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = Square(width=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.PermiterOfMySquare())
