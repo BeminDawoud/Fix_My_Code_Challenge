11b74358a640e14e49f1f008900d0997c9354a67
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-15 20:01:11 +0300

    update

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-"""class Square"""
+""" Module for square class"""
 
 
-class square:
-    """Class Square"""
-
+class Square():
+    """ Square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """Instantiation of class"""
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """Area of the square"""
+        """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """Permiter of the square"""
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """string representation of the square"""
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ Main entry point """
-    s = square(width=12, height=9)
+    """ Create a square object """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
