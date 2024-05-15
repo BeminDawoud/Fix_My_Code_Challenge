d11ea45ef4da2d0d4f9d7b098f148079408ddae1
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-15 19:27:07 +0300

    fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 
 
 class square:
+    """class of a square or a rectabgle"""
 
     width = 0
     height = 0
@@ -12,40 +13,12 @@ class square:
 
     def area_of_my_square(self):
         """Area of the square"""
-        if self.width == 0 and self.height == 0:
-            return 0
-
-        if self.width == 0:
-            return self.height * self.height
-
-        if self.height == 0:
-            return self.width * self.width
-
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """Perimeter of the square"""
-        if self.width == 0 and self.height == 0:
-            return 0
-
-        if self.width == 0:
-            return 4 * self.height
-
-        if self.height == 0:
-            return 4 * self.width
-
-        return 2 * (self.width + self.height)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        if self.width == 0 and self.height == 0:
-            return "0/0"
-
-        if self.width == 0:
-            return "0/{}".format(self.height)
-
-        if self.height == 0:
-            return "{}/0".format(self.width)
-
         return "{}/{}".format(self.width, self.height)
 
 
